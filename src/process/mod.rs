49101bfa6c54838e2d33aebf193d79ae8b88afbb
xooseph <josephlsalgado@outlook.com>
2025-05-03 17:27:19 -0600

    update: View style for process

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,20 +1,23 @@
 use gtk::prelude::*;
-use gtk::{glib, CssProvider};
+use gtk::CssProvider;
 use relm4::gtk::{
     gdk::Texture,
     gdk_pixbuf::Pixbuf,
     gio::{Cancellable, MemoryInputStream},
-    StyleContext,
+    glib, StyleContext,
 };
 use relm4::prelude::*;
-use glib::clone;
-
-use sysinfo::{
-    Components, Disks, Networks, System,
-};
 
 mod process_info; // Importar el módulo process_info
 use process_info::get_processes_info; // Importar la función
+
+pub fn embedded_logo(picture: &str) -> Texture {
+    let bytes = std::fs::read(picture).expect("Failed to read image file");
+    let g_bytes = glib::Bytes::from(&bytes);
+    let stream = MemoryInputStream::from_bytes(&g_bytes);
+    let pixbuf = Pixbuf::from_stream(&stream, Cancellable::NONE).unwrap();
+    Texture::for_pixbuf(&pixbuf)
+}
 
 #[derive(Default)]
 pub struct Process {
@@ -35,35 +38,6 @@ impl SimpleComponent for Process {
         let processes = get_processes_info(); // Usar la función para obtener los procesos
         let model = Process { processes };
 
-        // Crear dinámicamente las filas de la lista
-        let list_box = gtk::ListBox::new();
-        
-        for process in &model.processes {
-            let row = gtk::ListBoxRow::new();
-            let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 10);
-    
-            // Etiqueta para mostrar el nombre del proceso
-            let label = gtk::Label::new(Some(process));
-            label.set_margin_all(5);
-    
-            // Botón para cada proceso
-            let button = gtk::Button::with_label("X");
-            button.connect_clicked(clone!(@strong process => move |_| {
-                println!("Button clicked for process: {}", process);
-            }));
-    
-            // Agregar la etiqueta y el botón al contenedor horizontal
-            hbox.append(&label);
-            hbox.append(&button);
-    
-            // Establecer el contenedor como hijo de la fila
-            row.set_child(Some(&hbox));
-            list_box.append(&row);
-        }
-
-        root.append(&gtk::Label::new(Some("Process Administrator")));
-        root.append(&list_box);
-        
         let widgets = view_output!();
 
         let provider = CssProvider::new();
@@ -73,15 +47,131 @@ impl SimpleComponent for Process {
             &provider,
             gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
         );
-        
+
         relm4::ComponentParts { model, widgets }
     }
 
     view! {
         gtk::Box {
-            add_css_class: "process",
+            add_css_class: "cleaner_box",
             set_orientation: gtk::Orientation::Vertical,
-            set_spacing: 10,     
+
+            append = &gtk::Label {
+                add_css_class: "principal_text",
+                set_label: "Blitz System Cleaner",
+            },
+
+            append = &gtk::Box {
+                add_css_class: "principal_box",
+                set_orientation: gtk::Orientation::Vertical,
+
+                append = &gtk::Box {
+                    add_css_class: "process_box",
+                    set_orientation: gtk::Orientation::Horizontal,
+
+                    append = &gtk::Picture {
+                        set_paintable: Some(&embedded_logo("src/assets/64x64/process.png")),
+                    },
+
+                    append = &gtk::Label {
+                        add_css_class: "process_text",
+                        set_label: "Process Administrator",
+                    },
+                },
+
+                append = &gtk::Button {
+                    add_css_class: "process_button",
+                    set_label: "Kill Process(es) (0)",
+
+                    connect_clicked => move |_| {
+                        println!("Kill process list clicked");
+                    },
+                },
+
+                append = &gtk::Box {
+                    add_css_class: "process_info_box",
+                    set_orientation: gtk::Orientation::Vertical,
+
+                    append = &gtk::Box {
+                        add_css_class: "process_header_box",
+                        set_orientation: gtk::Orientation::Horizontal,
+
+                        append = &gtk::Picture {
+                            add_css_class: "process_info_icon",
+                            set_paintable: Some(&embedded_logo("src/assets/64x64/storage.png")),
+                        },
+
+                        append = &gtk::Label {
+                            add_css_class: "process_info_name_text",
+                            set_label: "Chrome",
+                        },
+
+                        append = &gtk::Box {
+                            set_hexpand: true,
+                            set_halign: gtk::Align::Fill,
+                        },
+
+                        append = &gtk::Button {
+                            add_css_class: "kill_process_button",
+                            set_child: Some(&gtk::Image::from_icon_name("window-close")),
+
+                            connect_clicked => move |_| {
+                                println!("Kill Process button clicked");
+                            },
+                        }
+                    },
+
+                    append = &gtk::Box {
+                        add_css_class: "process_info_tag_box",
+                        set_orientation: gtk::Orientation::Horizontal,
+
+                        append = &gtk::Label {
+                            add_css_class: "process_info_tag_text",
+                            set_label: "CPU:",
+                        }
+                    },
+
+                    append = &gtk::Box {
+                        add_css_class: "process_info_tag_box",
+                        set_orientation: gtk::Orientation::Horizontal,
+
+                        append = &gtk::Label {
+                            add_css_class: "process_info_tag_text",
+                            set_label: "Memory:",
+                        },
+
+                        append = &gtk::Box {
+                            set_hexpand: true,
+                            set_halign: gtk::Align::Fill,
+                        },
+
+                        append = &gtk::Label {
+                            add_css_class: "process_info_value_text",
+                            set_label: "1.2 GB",
+                        }
+                    },
+
+                    append = &gtk::Box {
+                        add_css_class: "process_info_tag_box",
+                        set_orientation: gtk::Orientation::Horizontal,
+
+                        append = &gtk::Label {
+                            add_css_class: "process_info_tag_text",
+                            set_label: "Type:",
+                        },
+
+                        append = &gtk::Box {
+                            set_hexpand: true,
+                            set_halign: gtk::Align::Fill,
+                        },
+
+                        append = &gtk::Label {
+                            add_css_class: "process_info_type_text",
+                            set_label: "Browser",
+                        }
+                    }
+                }
+            }
         }
     }
 }
